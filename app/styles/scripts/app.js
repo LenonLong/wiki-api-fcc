4fdf2bf69e179c28793215c0139283f0194026a8
Amrong Lenon Long <amronglong@gmail.com>
2017-09-05 11:22:25 -0700

    updated. push to github

---
+++ app/styles/scripts/app.js
@@ -0,0 +1,36 @@
+// Run jquery
+$(document).ready(function() {
+  //When search is clicked run code
+  $('#search').click(function() {
+    // Gets search input
+    var searchTerm = $('#searchTerm').val();
+    // API url with searchTerm
+    var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ searchTerm + "&format=json&callback=?";
+
+    $.ajax({
+      url: url,
+      type: "GET",
+      async: false,
+      dataType: "json",
+      success: function(data){
+        // data [1]array [0]first item
+        // console.log(data[1][0]);
+        // console.log(data[2][0]);
+        // console.log(data[3][0]);
+        $('#output').html('');
+      for(var i = 0; i < data[1].length; i++) {
+            $('#output').prepend("<li><a href="+data[3][i]+">"+data[1][i]+"</a><p>"+data[2][i]+"</p><li>");
+      }
+      $("#searchTerm").val('');
+      },
+      error: function(errorMessage){
+        alert("Error");
+      }
+    });
+  });
+  $("#searchTerm").keypress(function(e) {
+    if(e.which == 13) {
+      $("#search").click();
+    }
+  });
+});
